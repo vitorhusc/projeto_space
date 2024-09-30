1a343391c7e5690b62d72babd57174f8204d586e
vitorhusc <vitor.hs.cavalheiro@gmail.com>
2024-09-30 11:22:38 -0300

    Atualizando imagens, legendas e adicionando banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
+dados = {
+    1: {"nome": "Nebolusa de Carina", "legenda": "webbtelecopo.org/ Nasa /James Webb"},
+    2: {"nome": "Galaxia NGC 1079", "legenda": "nasa.org/ Nasa /Hubble"}
+}
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.all()
+    
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem (request):
-    return render(request, 'galeria/imagem.html')
+def imagem (request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
