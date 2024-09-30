b54a1056597ad7fbc8adfa4c373aee6df4cad370
vitorhusc <vitor.hs.cavalheiro@gmail.com>
2024-09-30 15:24:56 -0300

    Adicionando novas opcões no admin e configurando banco de dados para receber dados direto do admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,24 @@
 from django.db import models
+from datetime import datetime
+
 
 class Fotografia(models.Model):
+    
+    opcoes_categoria = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALAXIA", "Galaxia"),
+        ("PLANETA", "Planeta"),
+    ]
+    
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=opcoes_categoria, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank = False)
+    
     
 def __str__(self):
-    return f"Fotografia [nome= {self.nome}]"
+    return self.nome
--- galeria/views.py
+++ galeria/views.py
@@ -7,7 +7,9 @@ dados = {
 }
 
 def index(request):
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada = True)
+    
+    
     
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
