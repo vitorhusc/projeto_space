a05cbf19d4c3d067de5c921b38018e6491de4a2a
vitorhusc <vitor.hs.cavalheiro@gmail.com>
2024-10-21 11:49:48 -0300

    Adicinado metodo de cadastrar, editar ou deletar imagem

---
+++ apps/galeria/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
+from django.contrib import messages
+
+dados = {
+    1: {"nome": "Nebolusa de Carina", "legenda": "webbtelecopo.org/ Nasa /James Webb"},
+    2: {"nome": "Galaxia NGC 1079", "legenda": "nasa.org/ Nasa /Hubble"}
+}
+
+def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuario nao logado")
+        return redirect ('login')
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada = True)
+    
+    
+    
+    return render(request, 'galeria/index.html', {"cards": fotografias})
+
+def imagem (request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuario nao logado")
+        return redirect ('login')
+    
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar: 
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render (request, "galeria/buscar.html", {"cards": fotografias})
+
+def nova_imagem (request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuario nao logado")
+        return redirect ('login')
+    
+    form = FotografiaForms
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada!')
+            return redirect ('index')
+        
+    return render (request, 'galeria/nova_imagem.html', {'form':form})
+
+def editar_imagem (request, foto_id):
+    fotografia = Fotografia.objects.get(id= foto_id)
+    form = FotografiaForms(instance=fotografia)
+    
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso')
+            return redirect ('index')
+    
+    return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
+
+def deletar_imagem (request, foto_id):
+    fotografia = Fotografia.objects.get(id= foto_id)
+    fotografia.delete()
+    messages.success(request, 'Fotografia deletada com sucesso')
+    return redirect ('index')
+    
